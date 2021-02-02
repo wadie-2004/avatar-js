9a3378649773f14776a83c1bb32343809453d6f7
Flex <70218824+wadie-2004@users.noreply.github.com>
2021-02-02 18:52:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const Discord = require("discord.js");
 const fs = require("fs");
 var Jimp = require('jimp');
-const config = require("./config.json");
 var client = new Discord.Client();
 const express = require("express");
 const app = express();
@@ -21,33 +20,21 @@ client.on('ready', () => {
 	});
 });
 
-client.on("message", (message) => {
-    const args = message.content
-    .slice(config.prefix.length)
-    .trim()
-    .split(/ +/g);
-
-    if (!message.content.startsWith(config.prefix)) return;
-
-    const cmd = args.shift().toLowerCase();
-
-    if( cmd == "change" ){
-        var sentimg = (message.attachments)
-        if ( !sentimg.array()[0] ) return message.channel.send("No image specified");
-        var xext = sentimg.array()[0].url.split(".")
-        var ext = xext[xext.length-1]
-    
-        Jimp.read( sentimg.array()[0].url )
-        .then(image => {
-            image.greyscale().write('./images/last.'+ext);
-            message.channel.send({
-                files: ["./images/last."+ext]
+const canvacord = require('canvacord')
+client.on("message", async msg => {
+    if (msg.content === ".change") {
+        let img;
+        if (msg.attachments.size > 0) {
+            img = msg.attachments.first().url
+        } else {
+            img = msg.author.displayAvatarURL({
+                format: 'png',
+                size: 1024
             })
-        })
-        .catch(err => {
-            message.channel.send(`Failed to greystyle the image\nError\n\`\`\`\n${err}\n\`\`\``)
-        });
+        }
+        const image = await canvacord.Canvas.greyscale(img);
+        const attachment = new Discord.MessageAttachment(image, "greyscale.png");
+        return msg.channel.send(attachment);
     }
 });
-
-client.login(config.token)
+client.login("")
